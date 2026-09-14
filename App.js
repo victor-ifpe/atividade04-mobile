e94ffb756d2b535231da2488d63231ecaec846e0
victor-ifpe <vgs13@discente.ifpe.edu.br>
2026-09-14 19:52:16 -0300

    versao01

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import TelaLogin from './src/Telas/TelaLogin';
+import TelaListaContatos from './src/Telas/TelaListaContatos';
+import TelaCadastroUsuario from './src/Telas/TelaCadastroUsuario';
+import TelaCadastroContato from './src/Telas/TelaCadastroContato';
+import TelaEdicaoContatos from './src/Telas/TelaEdicaoContatos';
+import TelaEsqueceuSenha from './src/Telas/TelaEsqueceuSenha';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="TelaLogin">
+
+        <Stack.Screen
+          name="TelaLogin"
+          component={TelaLogin}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="TelaListaContatos"
+          component={TelaListaContatos}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="TelaCadastroUsuario"
+          component={TelaCadastroUsuario}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="TelaCadastroContato"
+          component={TelaCadastroContato}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="TelaEdicaoContatos"
+          component={TelaEdicaoContatos}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="TelaEsqueceuSenha"
+          component={TelaEsqueceuSenha}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,11 @@
+# Lista de Comandos
+
+## Lista de Instalações
+
+* npx create-expo-app atividade04-mobile --template bare-minimum@sdk-57
+* npm install axios
+* npm install firebase
+
+## Lista de Inicializações
+
+* npx expo start -c --tunnel
